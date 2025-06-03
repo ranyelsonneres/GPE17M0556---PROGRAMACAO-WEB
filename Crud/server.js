7fea8359292fde733c58357730f9796a87314ea2
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-06-03 11:13:50 -0300

    Update server.js

--- Crud/server.js
+++ Crud/server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//disponibilizar o caminho dos arquivos estáticos
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 //criar a conexão com o banco
 const db = require('./db');
 
 //rotas
 //criar a rota padrão
 app.get('/', (req, res) =>{
-    res.send("Front Funcionando");
+    //res.send("Front Funcionando");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
 //cria a rotas para a listagem dos usuários
+app.use(express.json()); //lidar com dados relacionados ao json
 const apiRoutes = require('./routes/api');
 app.use('/api/users', apiRoutes);
 
